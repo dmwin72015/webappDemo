98def58847dc44d76ae45788f38d3530709e3e74
dongmin_2 <dong2013job@163.com>
2016-03-25 18:39:40 +0800

    新增后台管理

---
+++ 后台界面/js/modules/index.js
@@ -0,0 +1,16 @@
+define(function(require, exports, module) {
+	var week_cn = '日一二三四五六';
+    // 通过 require 引入依赖
+    var $ = require('jquery');
+
+    function getDate_cn() {
+
+    }
+    // 通过 exports 对外提供接口
+    exports.doSomething = ...
+
+
+    // 或者通过 module.exports 提供整个接口
+    module.exports = ...
+
+});
